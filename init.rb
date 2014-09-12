43b6c58d3b1d31dd52cdc4f4af4b8d3df9dae6ed
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2014-09-12 10:08:56 +0200

    commented init querypatch

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require 'redmine_spent_time_column/hooks'
 
 ActionDispatch::Callbacks.to_prepare do
   Issue.send(:include, RedmineSpentTimeColumn::Patches::IssuePatch) unless Issue.include?(RedmineSpentTimeColumn::Patches::IssuePatch)
-  Query.send(:include, RedmineSpentTimeColumn::Patches::QueryPatch) unless Query.include?(RedmineSpentTimeColumn::Patches::QueryPatch)
+  # Query.send(:include, RedmineSpentTimeColumn::Patches::QueryPatch) unless Query.include?(RedmineSpentTimeColumn::Patches::QueryPatch)
   QueriesHelper.send(:include, RedmineSpentTimeColumn::Patches::QueriesHelperPatch) unless QueriesHelper.include?(RedmineSpentTimeColumn::Patches::QueriesHelperPatch)
 end
 
